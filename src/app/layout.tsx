066b5a2784b76f5ef3eab7778bbf4fa4e73e416e
Steve Leinweber <steve@beelinemicrosystems.com>
2025-09-13 10:41:26 -0500

    init: Company Name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,16 @@ import { revalidateSyncTags } from '@/sanity/revalidateSyncTags'
 import '@/styles/tailwind.css'
 import type { Metadata } from 'next'
 
-export const metadata: Metadata = {
+/* export const metadata: Metadata = {
   title: {
     template: '%s - Radiant',
     default: 'Radiant - Close every deal',
   },
+} */
+
+export const metadata = {
+  title: "Bee Line Microsystems",
+  description: "Custom AI + SaaS solutions for Chicago businesses"
 }
 
 export default function RootLayout({
