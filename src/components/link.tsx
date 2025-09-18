28e33450e03e5117e33485aca758b79984067d3c
Steve Leinweber <steve@beelinemicrosystems.com>
2025-09-18 14:52:33 -0500

    fix: link.ts DataInteractive tag

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -7,7 +7,7 @@ export const Link = forwardRef(function Link(
   ref: React.ForwardedRef<HTMLAnchorElement>,
 ) {
   return (
-    <Headless.DataInteractive>
+    <Headless.DataInteractive as="div">
       <NextLink ref={ref} {...props} />
     </Headless.DataInteractive>
   )
